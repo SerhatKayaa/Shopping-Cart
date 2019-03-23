91cedae6b1322e7bc0e663acfbcd165036ff017b
skaya2E <serhat.kayaa3@gmail.com>
2019-03-23 13:28:21 +0300

    Create api and data

---
+++ api/data.js
@@ -0,0 +1,47 @@
+const products = [
+    {
+      id: 1,
+      name: 'Cool Vex',
+      available_quantity: 5,
+      price: 450,
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+    },
+   
+    {
+      id: 2,
+      name: 'Vintage Ties',
+      available_quantity: 7,
+      price: 50,
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+    },
+   
+    {
+      id: 3,
+      name: 'Sunglasses',
+      available_quantity: 0,
+      price: 500,
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+    },
+   
+    {
+      id: 4,
+      name: 'Wrist Watch',
+      available_quantity: 4,
+      price: 1500,
+      description: 'Lorem ipsum dolor sit amet, iusto appellantur vix te, nam affert feugait menandri eu. Magna simul ad est. Nostrum neglegentur ius at, at pertinax repudiare vel. Vim an adolescens quaerendum.'
+    },
+  ];
+   
+  const users = [
+      {
+        'name': 'user',
+        'password': 'qwerty'
+      },
+      {
+        'name': 'example',
+        'password': 'qwerty'
+      }
+  ];
+   
+  module.exports = { 'products': products, users: users }
+  
