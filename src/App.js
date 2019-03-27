8873c1518077a3dc1be0e702e1b3c03e003e911b
skaya2E <serhat.kayaa3@gmail.com>
2019-03-27 14:49:43 +0300

    login-form Create login form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Login from './components/Login';
 import './App.css';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { isAuthenticated } from './repository';
 
 class App extends Component {
   render() {
+    const auth = isAuthenticated();
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Route exact path='/' component ={Login} />
+        </div>
+      </Router>
     );
   }
 }
